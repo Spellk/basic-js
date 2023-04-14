7a8469d245622f9d5ccae2a49d95c3f388d2f552
Artem Lebedev <lebedevam91@mail.ru>
2023-04-14 12:16:30 +0300

    Revert "solution"
    This reverts commit a52e86998251e9ff155ce42d9321228b584d3261.

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require("../extensions/index.js");
+const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given an email address, return it's domain.
@@ -10,10 +10,11 @@ const { NotImplementedError } = require("../extensions/index.js");
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(email) {
-  return email.split("@").pop();
+function getEmailDomain(/* email */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
-  getEmailDomain,
+  getEmailDomain
 };
